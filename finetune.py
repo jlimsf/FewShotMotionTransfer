197b3f8f604316a868d0fbafb68d684e924e3ca5
john.lim <ubuntu@ip-10-0-6-186.ec2.internal>
2021-08-12 16:55:05 +0000

    add 256

--- finetune.py
+++ finetune.py
@@ -177,8 +177,8 @@ def inference(model, config, device_idxs=[0]):
 
                 print (class_body_full)
                 print (class_body_full.shape)
-                cv2.imwrite('full_class_bodydebug.png', class_body_full)
-                cv2.imwrite('full_class_bodydebugsmall.png', class_body_resize)
+                cv2.imwrite('full_class_bodydebug_256.png', class_body_full)
+                cv2.imwrite('full_class_bodydebugsmall_256.png', class_body_resize)
                 print ('saving')
                 exit()
                 fake_image = np.clip(fake_image.cpu().numpy(), 0, 1)
